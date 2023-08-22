68776f860176e0f16f5095b13717a48aa70dabb1
Naveen Kumar <101378030+Nightcrawler1256@users.noreply.github.com>
2023-08-22 17:55:25 +0530

    Difference of length and length()

---
+++ L123.java
@@ -0,0 +1,19 @@
+public class L123 {
+    public static void main(String[] args) {
+        int a[]={1,3,4,6};
+        System.out.println(a.length);
+
+        int b[][]={
+                {1,4,5},
+                {2,5},
+                {3,5,7,9}
+        };
+        for (int i=0;i<3;i++)
+        {
+            System.out.println(b[i].length +" ");
+        }
+
+        String name="navin";
+        System.out.println(name.length());
+    }
+}
