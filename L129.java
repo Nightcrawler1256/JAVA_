0c82e8c21f2dcd76e3fcd5937eb295f48828f525
Naveen Kumar <101378030+Nightcrawler1256@users.noreply.github.com>
2023-08-23 23:50:39 +0530

    mutable and immutable string program

---
+++ L129.java
@@ -0,0 +1,13 @@
+public class L129 {
+    public static void main(String[] args) {
+
+        StringBuffer s =new StringBuffer("GOOD");
+        s.append(" MORNING");
+        System.out.println(s);
+
+        String sc=new String("good");
+        sc=sc+" morning";
+        System.out.println(sc);
+
+    }
+}
