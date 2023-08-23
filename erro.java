ce4c263d3eefab35e8a78f4e1e2960338e77d353
Naveen Kumar <101378030+Nightcrawler1256@users.noreply.github.com>
2023-08-23 23:54:47 +0530

    Array value increase once it is initialize

---
+++ erro.java
@@ -0,0 +1,7 @@
+public class erro {
+    public static void main(String[] args) {
+        int arr[]=new int[5];
+        arr=new int[6];
+        System.out.println("array increment during the program coding");
+    }
+}
