442e493b8dd0351e789d86618082845ea8e08c2c
Naveen Kumar <101378030+Nightcrawler1256@users.noreply.github.com>
2023-08-23 23:35:49 +0530

    String types code and there comparison program

---
+++ L128.java
@@ -0,0 +1,16 @@
+public class L128 {
+    public static void main(String[] args) {
+        String name="navin";
+        System.out.println(name);
+        String s=new String("navin kumar");
+        System.out.println(s);
+
+
+        String s1="hello";
+        String s2="hello";
+        String s3= new String("hello");
+        String s4=new String("hello");
+        System.out.println(s1==s2);
+        System.out.println(s3==s4);
+    }
+}
